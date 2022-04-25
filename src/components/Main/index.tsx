c75d1370a53be94a39c02296631e492ae6ef6aee
Rômulo Ramos <rmsramos@gmail.com>
2022-04-25 11:17:54 -0300

    Atualization Main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,13 @@
-const Main = () => (
-  <main>
-    <h1>My first component</h1>
-  </main>
-)
+import { Flex, Text } from '@chakra-ui/react'
+
+const Main = () => {
+  return (
+    <Flex w={'100vw'} h={'100vh'} align={'center'} justify={'center'}>
+      <h1>
+        <Text>My First Component</Text>
+      </h1>
+    </Flex>
+  )
+}
 
 export default Main
