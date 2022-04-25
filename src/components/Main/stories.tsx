c9422748f293c0683402cf137ce0cb0b49e5fa2a
Rômulo Ramos <rmsramos@gmail.com>
2022-04-24 22:11:34 -0300

    FIX: package.json jest.config stories.tsx(Main)

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,8 +1,10 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
 import Main from '.'
 
 export default {
   title: 'Main',
   component: Main
-}
+} as ComponentMeta<typeof Main>
 
-export const Basic = () => <Main />
+export const Primary: ComponentStory<typeof Main> = () => <Main></Main>
